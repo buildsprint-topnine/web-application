e20654b54cf11b40a95873091b9950029d6e42df
Leelsmuth <femianjorin@rocketmail.com>
2019-10-21 17:13:28 +0100

    added signup and login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, NavLink, withRouter, Redirect } from "react-router-dom";
 import * as actionCreators from "./state/actionCreators";
+import Login from "./components/Login";
+import Signup from "./components/SignUp";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Build</h1>
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
