256389179305d1f2210293e199b51ea2b860c17d
Leelsmuth <femianjorin@rocketmail.com>
2019-10-22 16:23:03 +0100

    created the FriendHome component

--- src/state/actionCreators.js
+++ src/state/actionCreators.js
@@ -1,44 +1,65 @@
-import axiosWithAuth from '../utils/axiosWithAuth'
-import * as types from './actionTypes'
+import axiosWithAuth from "../utils/axiosWithAuth";
+import * as types from "./actionTypes";
 
-export function fetchItems(){
-    return function(dispatch){
+export function fetchItems() {
+  return function(dispatch) {
+    dispatch({
+      type: types.FETCHING_ITEM_START
+    });
+    axiosWithAuth()
+      .get(`https://bw-my-top-nine.herokuapp.com/home`)
+      .then(res => {
         dispatch({
-            type: types.FETCHING_ITEM_START
-        })
-        axiosWithAuth()
-        .get(`https://bw-my-top-nine.herokuapp.com/home`)
-        .then(res=>{
-            dispatch({
-                type:types.FETCH_ITEM_SUCCESS,
-                payload:res.data
-            })
-        })
-        .catch(error=>{
-            dispatch({
-                type:types.FETCH_ITEM_FAILURE,
-                payload:error
-            })
-        })
-    }
+          type: types.FETCH_ITEM_SUCCESS,
+          payload: res.data
+        });
+      })
+      .catch(error => {
+        dispatch({
+          type: types.FETCH_ITEM_FAILURE,
+          payload: error
+        });
+      });
+  };
 }
 
-
 export function addFriend(char) {
-    return function(dispatch) {
-      axiosWithAuth()
-        .post("https://bw-my-top-nine.herokuapp.com/home/add-top-nine", char)
-        .then(res =>
-          dispatch({
-            type: types.ADD_ITEM_SUCCESS,
-            payload: res.data
-          })
-        )
-        .catch(error =>
-          dispatch({
-            type: types.ADD_ITEM_FAILURE,
-            payload: error
-          })
-        );
-    };
-  }
+  return function(dispatch) {
+    axiosWithAuth()
+      .post("https://bw-my-top-nine.herokuapp.com/home/add-top-nine", char)
+      .then(res =>
+        dispatch({
+          type: types.ADD_ITEM_SUCCESS,
+          payload: res.data
+        })
+      )
+      .catch(error =>
+        dispatch({
+          type: types.ADD_ITEM_FAILURE,
+          payload: error
+        })
+      );
+  };
+}
+
+export function fetchFriendItems() {
+  return function(dispatch) {
+    dispatch({
+      type: types.FETCHING_ITEM_START
+    });
+    axiosWithAuth()
+      .get(`https://bw-my-top-nine.herokuapp.com/home`)
+      .then(res => {
+        dispatch({
+          type: types.FETCH_ITEM_SUCCESS,
+          payload: res.data
+        });
+      })
+      .catch(error => {
+        dispatch({
+          type: types.FETCH_ITEM_FAILURE,
+          payload: error
+        });
+      });
+  };
+}
